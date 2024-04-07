69cc41b009862c8ed23cb97153851c59a2e055de
= <=>
2024-04-07 16:31:30 +0200

    Entrega practica 2 como dice el enunciado

--- Practica 2/Contador/src/App.jsx
+++ Practica 2/Contador/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import Contador from './componentes/contador';
+import React, { useState } from 'react';
+import Boton from '../src/componentes/Boton';
+import Contador from '../src/componentes/contador';
 
-function App() {
+const App = () => {
+  const [numClics, setNumClics] = useState(0);
+
+  const incrementNum = () => {
+    console.log("Suma 1");
+    setNumClics(prevNumClics => prevNumClics + 1);
+  };
+
+  const reiniciarNum = () => {
+    console.log("Reinicia a 0");
+    setNumClics(0);
+  };
 
   return (
-    <>
-      <Contador />
-    </>
-  )
-}
+    <div>
+      <Contador numClicks={numClics}/>
+      <Boton text="Click" onClick={incrementNum} esClick={true} />
+      <Boton text="Reiniciar" onClick={reiniciarNum} esClick={false} />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- Practica 2/Contador/src/componentes/contador.jsx
+++ Practica 2/Contador/src/componentes/contador.jsx
@@ -1,28 +1,11 @@
-import { useState } from 'react'
+import React from 'react';
 
+const Contador = ({ numClicks}) => {
+  return (
+    <div>
+      <p>Contador: {numClicks}</p>
+    </div>
+  );
+};
 
-const contador=()=> {
-    const [count, setCount] = useState(0)
-
-    const incrementNum =()=>{
-        setCount(count+1)
-        console.log("Incrementa 1")
-
-    }
-
-    const reiniciarNum =()=>{
-        setCount(0)
-        console.log("Reiniciado a 0")
-    }
-
-    return (
-      <>
-        <h1>Contador</h1>
-        <h2>{count}</h2>
-        <button onClick={incrementNum}>Sumar</button>
-        <button onClick={reiniciarNum}>Reiniciar</button>
-      </>
-    )
-  }
-  
-  export default contador
+export default Contador;
