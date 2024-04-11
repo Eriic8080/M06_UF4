02c6e9fae36bcf67ca943c4d497eeb3a35db3c76
= <=>
2024-04-12 00:54:34 +0200

    Componente que crea tareas y las introduce al array para poder mostrar-lo desde el componente llistaTasques

--- Practica 3/LlistaDeTasques/src/componentes/FormulariTasques.jsx
+++ Practica 3/LlistaDeTasques/src/componentes/FormulariTasques.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
 import './estilos.css'
-const Formulario =()=>{
+function Formulario ({addTodo}){
+    const [userInput, setUserInput] = useState('');
+
+    //Aqui manejo el evento de onChange para poder sobreescribir el input
+    const handleChange=(e)=>{
+        setUserInput(e.currentTarget.value);
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        if(userInput.trim() != ''){
+            addTodo(userInput);
+            setUserInput('');
+        }
+    }
 
     return(
         <>
             <div>
-                <input className="barraDatos" placeholder='Introduce la tarea'></input>
-                <button className='enviarTarea'>Agregar Tarea</button>
+                <form onSubmit={handleSubmit}>
+                    <input className="barraDatos" placeholder='Introduce la tarea' value={userInput} onChange={handleChange}></input>
+                    <button className='enviarTarea'>Agregar Tarea</button>
+                </form>
             </div>
         </>
     )
