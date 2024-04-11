4979bed8e8c95e9b280ab624bb8aa8bdbf1c0efc
= <=>
2024-04-12 00:53:18 +0200

    Componente de cada tarea individual

--- Practica 3/LlistaDeTasques/src/componentes/Tasca.jsx
+++ Practica 3/LlistaDeTasques/src/componentes/Tasca.jsx
@@ -1,11 +1,22 @@
 import './estilos.css'
 
-const Tarea =()=>{
+function Tarea ({todo, onComplete, onDeleteItem}){
+
+    //Esta funcion hace que al clicar el texto de la tarea llame tambien a la funcion onComplete para marcar-la como completada
+    const handleClicarTexto = () => {
+        onComplete(todo.id);
+      };
+    
+
     return(
-        <>
+        <>  
             <div className="recuadroTarea">
-                <h4 className='textoTarea'>TAREA 1</h4>
-                <button className='eliminarTarea'>X</button>
+                {/** En el input indico que mientra el dato completed sea true este marcado el input */}
+                <input type='checkbox' checked={todo.completed} onChange={() => onComplete(todo.id)}/>
+                <h4 className={todo.completed ? 'completed' : 'not-completed'} onClick={handleClicarTexto}>{todo.task}</h4>
+                <button className='eliminarTarea' onClick={() =>onDeleteItem(todo.id)}>
+                <img src='../src/assets/delete_tarea.png' alt='X' className='iconoBasura'></img>
+                </button>
             </div>
         </>
     )
