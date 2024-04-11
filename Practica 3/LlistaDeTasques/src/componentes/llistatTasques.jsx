d21abcf65e575996c350ab40d0d30c8e39f58a2a
= <=>
2024-04-12 00:53:49 +0200

    Componente que muestra todas las tareas creadas

--- Practica 3/LlistaDeTasques/src/componentes/llistatTasques.jsx
+++ Practica 3/LlistaDeTasques/src/componentes/llistatTasques.jsx
@@ -1,10 +1,18 @@
+import Tarea from './Tasca'
 
-function llista({children}) {
+function llista({todos, onComplete, onDeleteItem}) {
   
     return (
       <>
         <div>
-            {children}
+            {
+            /*   Aqui recibo los valores conel objeto todos y con el map voy iterando cada objeto y los voy llevando 
+                  al componente Tarea para mostrar-lo con una key para identificar de manera unica cada objeto          */ 
+              todos.map((todo ,index) => (
+                <Tarea key={index} todo={todo} onComplete={onComplete} onDeleteItem={onDeleteItem} />
+
+              ))
+            }
         </div>
       </>
     )
